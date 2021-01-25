18b7953d8bd91fba92e12bd071a7782a149030d6
Hilton <hdmagno@gmail.com>
2021-01-25 16:34:00 -0300

    Entidades e repositórios

---
+++ ExcercicioMvc01.Repositoy/Contracts/IBaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExcercicioMvc01.Repositoy.Contracts
+{
+    public interface IBaseRepository<T>
+    {
+        void Inserir(T obj);
+        void Atualizar(T obj);
+        void Excluir(T obj);
+        List<T> BuscarTodos();
+        T BusrcarPorId(Guid id);
+        List<T> BuscarPorNome(string nome);
+    }
+}
